1bf63a16183e5d3cf6cb44f2bfe3eaf753a9b14e
marshallmueller <mashymue@gmail.com>
2021-07-04 22:15:24 -0500

    replaced jekyll with more vanilla format

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const content = document.querySelector < HTMLDivElement > ('#content')
+
+content.innerHTML = `
+  <div>
+      <h1>Links</h1>
+      <ul>
+        <li><a href="../index.html">home</a></li>
+        <li><a href="../stuff.html">stuff</a></li>
+        <li> <a href="../about.html">about</a> </li>
+      </ul>
+    </div>
+    `
