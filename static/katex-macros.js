903cd50c2a51b33673bb8af7fa4c58ea86976e78
marshallmueller <mashymue@gmail.com>
2025-08-19 20:12:11 -0700

    Added Katex Support

---
+++ static/katex-macros.js
@@ -0,0 +1,14 @@
+// KaTeX macros for mathematical expressions
+window.katexMacros = {
+    // Common mathematical shortcuts
+    "\\R": "\\mathbb{R}",
+    "\\N": "\\mathbb{N}",
+    "\\Z": "\\mathbb{Z}",
+    "\\C": "\\mathbb{C}",
+    
+    // Calculus
+    "\\dd": "\\,\\mathrm{d}",
+    "\\partial": "\\partial",
+    "\\grad": "\\nabla",
+    
+};
